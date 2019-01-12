b3991b8b464acbc27a8d7cb041dba6d789bd33a4
zl <952797732@qq.com>
2019-01-12 13:04:26 +0800

    complete grid component

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,13 @@
 </template>
 
 <script>
+function validator (value,msg) {
+  let keys = Object.keys(value)
+  if (!keys.includes('span') && !keys.includes('offset')) {
+    console.warn(`col prop ${msg}(object) must have one key of [span,offset] at least!`)
+  }
+  return true
+};
 export default {
   name: "bear-col",
   data () {
@@ -13,12 +20,31 @@ export default {
     }
   },
   props: {
+    xs: {
+      type: Object,
+      validator
+    },
+    sm: {
+      type: Object,
+      validator
+    },
+    md: {
+      type: Object,
+      validator
+    },
+    lg: {
+      type: Object,
+      validator
+    },
     span: {
       type: [String, Number],
     },
     offset: {
       type: [String, Number],
     },
+    className:{
+      type:String
+    }
   },
   computed: {
     colStyle () {
@@ -29,11 +55,28 @@ export default {
       }
     },
     colClasses () {
-      let { span, offset } = this
+      let {
+        span,
+        offset,
+        className,
+        xs: { span: xsSpan, offset: xsOffset } = {},
+        sm: { span: smSpan, offset: smOffset } = {},
+        md: { span: mdSpan, offset: mdOffset } = {},
+        lg: { span: lgSpan, offset: lgOffset } = {},
+      } = this
       return [
         'b-col',
+        className && `${className}`,
         span && `b-col-${span}`,
-        offset && `b-offset-${offset}`
+        offset && `b-offset-${offset}`,
+        xsSpan && `b-xs-span-${xsSpan}`,
+        smSpan && `b-sm-span-${smSpan}`,
+        mdSpan && `b-md-span-${mdSpan}`,
+        lgSpan && `b-lg-span-${lgSpan}`,
+        xsOffset && `b-xs-offset-${xsOffset}`,
+        smOffset && `b-sm-offset-${smOffset}`,
+        mdOffset && `b-md-offset-${mdOffset}`,
+        lgOffset && `b-lg-offset-${lgOffset}`
       ]
     }
   }
@@ -51,6 +94,54 @@ export default {
   @for $m from 1 through 24 {
     &.b-offset-#{$m} {
       margin-left: ($m/24) * 100%;
+    }
+  }
+  @media screen and (min-width: 0px) {
+    @for $n from 1 through 24 {
+      &.b-xs-span-#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    @for $m from 1 through 24 {
+      &.b-xs-offset-#{$m} {
+        margin-left: ($m/24) * 100%;
+      }
+    }
+  }
+  @media screen and (min-width: 768px) {
+    @for $n from 1 through 24 {
+      &.b-sm-span-#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    @for $m from 1 through 24 {
+      &.b-sm-offset-#{$m} {
+        margin-left: ($m/24) * 100%;
+      }
+    }
+  }
+  @media screen and (min-width: 992px) {
+    @for $n from 1 through 24 {
+      &.b-md-span-#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    @for $m from 1 through 24 {
+      &.b-md-offset-#{$m} {
+        margin-left: ($m/24) * 100%;
+      }
+    }
+  }
+  @media screen and (min-width: 1200px) {
+    @for $n from 1 through 24 {
+      &.b-lg-span-#{$n} {
+        width: ($n/24) * 100%;
+      }
+    }
+    @for $m from 1 through 24 {
+      &.b-lg-offset-#{$m} {
+        margin-left: ($m/24) * 100%;
+      }
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="b-row" :style='rowStyle'>
+  <div class="b-row" :style='rowStyle' :class='rowClasses'>
     <slot></slot>
   </div>
 </template>
@@ -10,7 +10,27 @@ export default {
   props: {
     gutter: {
       type: [String, Number]
-    }
+    },
+    justify:{
+      type: String,
+      validator(value){
+        let valueArray = ['start','end','center','space-around','space-between']
+        if(valueArray.indexOf(value) === -1){
+          console.warn(`b-row's prop justify must be one of [${valueArray}]`)
+        }
+        return true
+      }
+    },
+    align:{
+      type: String,
+      validator(value){
+        let valueArray = ['top','middle','bottom']
+        if(valueArray.indexOf(value) === -1){
+          console.warn(`b-row's prop justify must be one of [${valueArray}]`)
+        }
+        return true
+      }
+    },
   },
   mounted () {
     this.$children.forEach(vm => {
@@ -24,6 +44,13 @@ export default {
         marginLeft: -gutter / 2 + 'px',
         marginRight: -gutter / 2 + 'px'
       }
+    },
+    rowClasses () {
+      let {justify,align} = this
+      return [
+        justify && `b-justify-${justify}`,
+        align && `b-align-${align}`
+      ]
     }
   }
 }
@@ -32,5 +59,30 @@ export default {
 <style scoped lang="scss">
 .b-row {
   display: flex;
+  flex-wrap: wrap;
+}
+.b-justify-center{
+  justify-content: center;
+}
+.b-justify-start{
+  justify-content: flex-start;
+}
+.b-justify-end{
+  justify-content: flex-end;
+}
+.b-justify-around{
+  justify-content: space-around;
+}
+.b-justify-between{
+  justify-content: space-between;
+}
+.b-align-top{
+  align-items: flex-start;
+}
+.b-align-middle{
+  align-items: center;
+}
+.b-align-bottom{
+  align-items: flex-end;
 }
 </style>
