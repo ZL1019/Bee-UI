866423600e785954e9b75321b2031020ecc49410
zl <952797732@qq.com>
2019-06-11 12:35:46 +0800

    popover支持四个方位

--- src/popover.vue
+++ src/popover.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="b-popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="b-poppover-pop" v-if="show">
+    <div ref="contentWrapper" :class="classes" v-if="show">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <div ref="triggerWrapper" class="b-popover-trigger">
       <slot></slot>
-    </span>
+    </div>
   </div>
 </template>
 
@@ -19,18 +19,49 @@ export default {
     content: {
       type: [Number, String],
     },
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) > -1;
+      },
+    },
   },
   data() {
     return {
       show: false,
     };
   },
+  computed: {
+    classes() {
+      return {
+        'b-popover-content': true,
+        [`position-${this.position}`]: true,
+      };
+    },
+  },
   methods: {
     locateContent() {
       document.body.appendChild(this.$refs.contentWrapper);
-      let { left, top } = this.$refs.triggerWrapper.getBoundingClientRect();
-      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
-      this.$refs.contentWrapper.style.top = top + window.scrollY +'px';
+      let {
+        left,
+        top,
+        width,
+        height,
+      } = this.$refs.triggerWrapper.getBoundingClientRect();
+      if (this.position === 'top') {
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+      } else if (this.position === 'bottom') {
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + height + 'px';
+      } else if (this.position === 'left') {
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+      } else {
+        this.$refs.contentWrapper.style.left = left + window.scrollX + width + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+      }
     },
     listenDocument() {
       console.log('++ 新增监听器');
@@ -75,15 +106,64 @@ export default {
 <style lang="scss">
 .b-popover {
   position: relative;
-  border: 1px blue solid;
   top: 50px;
-  .b-poppover-trigger {
-    border: 1px green solid;
+  display: inline-block;
+}
+.b-popover-content {
+  position: absolute;
+  
+  border-radius: 4px;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+  padding: 8px 16px;
+  
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -8px;
+    &:before {
+      content: '';
+      display: block;
+      border: 6px transparent solid;
+      border-top-color: white;
+      position: absolute;
+      top: 100%;
+    }
+  }
+  &.position-bottom {
+    margin-top: 8px;
+    &:before {
+      content: '';
+      display: block;
+      border: 6px transparent solid;
+      border-bottom-color: white;
+      position: absolute;
+      bottom: 100%;
+    }
+  }
+  &.position-left {
+    margin-left: -8px;
+    transform: translateX(-100%);
+    &:before {
+      content: '';
+      display: block;
+      border: 6px transparent solid;
+      border-left-color: white;
+      position: absolute;
+      left:100%;
+    }
+  }
+  &.position-right {
+    margin-left: 8px;
+    &:before {
+      content: '';
+      display: block;
+      border: 6px transparent solid;
+      border-right-color: white;
+      position: absolute;
+      right:100%;
+    }
   }
 }
-.b-poppover-pop {
-  position: absolute;
-  border: 1px red solid;
-  transform: translateY(-100%);
+.b-popover-trigger {
+  display: inline-block;
 }
 </style>
