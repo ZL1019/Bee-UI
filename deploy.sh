3de7ef90219834fa5e987296a43f86300f5ac604
zl <mumuloveruby@gmail.com>
2019-06-28 13:25:46 +0800

    更改 deploy.sh 部署文件

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:NO2046/NO2046.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:NO2046/Bear-UI.git master:gh-pages
