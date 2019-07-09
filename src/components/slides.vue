2fb1b4fa76fd959efe4ee75698ae4795fbcdea5f
zl <mumuloveruby@gmail.com>
2019-07-09 18:45:11 +0800

    commit

--- src/components/slides.vue
+++ src/components/slides.vue
@@ -3,77 +3,85 @@
     {{this.selected}}
     <div class="b-slides-item-container">
       <slot></slot>
-    </div> 
+    </div>
   </div>
 </template>
 
 <script>
-import { setTimeout, setInterval } from 'timers';
 export default {
-  name:'bear-slides',
-  data(){
-    return {
-
-    }
+  name: 'bear-slides',
+  data() {
+    return {};
   },
-  props:{
-    selected:{
-      type: String
+  props: {
+    selected: {
+      type: String,
     },
-    time:{
+    time: {
       type: Number,
-      default: 1000
+      default: 1000,
     },
-    isAutoPlay:{
+    isAutoPlay: {
       type: Boolean,
-      default: true
-    }
-  },
-  mounted(){
-    if(!this.selected){
-      this.$children[0].visible = true
-    }else{
-      this.updateSelected()
-    }
-    if(this.isAutoPlay){
-      this.autoPlay()
-    }
-    
-  },
-  methods:{
-    autoPlay(){
-      let names = this.$children.map(child => child.name)
-      let index = names.findIndex(item => this.selected === item)
-      setInterval(()=>{
-        if(index === names.length-1){
-          index = 0
-        }else{
-          index = index + 1
-        }
-        this.$emit('update:selected',names[index])
- 
-      },this.time)
+      default: true,
     },
-    updateSelected(){
-      this.$children.forEach(child => {
-        child.visible = this.selected === child.name
-      })
+  },
+  mounted() {
+    if (!this.selected) {
+      this.$children[0].visible = true;
+    } else {
+      this.updateSelected();
+    }
+    if (this.isAutoPlay) {
+      this.autoPlay();
     }
   },
-  updated(){
-    this.updateSelected()
-  }
-}
+  methods: {
+    autoPlay() {
+      let names = this.$children.map(child => child.name);
+      let index = names.findIndex(item => this.selected === item);
+      let fn = () => {
+        if (index === names.length - 1) {
+          index = 0;
+        } else {
+          index = index + 1;
+        }
+        this.$emit('update:selected', names[index]);
+        setTimeout(() => {
+          fn();
+        }, this.time);
+      };
+      fn();
+      // setInterval(()=>{
+      //   if(index === names.length-1){
+      //     index = 0
+      //   }else{
+      //     index = index + 1
+      //   }
+      //   this.$emit('update:selected',names[index])
+
+      // },this.time)
+    },
+    updateSelected() {
+      this.$children.forEach(child => {
+        child.visible = this.selected === child.name;
+      });
+    },
+  },
+  updated() {
+    this.updateSelected();
+  },
+};
 </script>
 
 <style lang="scss">
-  .b-slides{
-    display: inline-block;
-    .b-slides-item-container{
-      display: flex;
-      overflow: hidden;
-      position: relative;
-      border:1px red solid;
-    }
+.b-slides {
+  display: inline-block;
+  .b-slides-item-container {
+    display: flex;
+    overflow: hidden;
+    position: relative;
+    border: 1px red solid;
   }
+}
 </style>
